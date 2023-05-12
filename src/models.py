14b673a1561110960b639c6477f08726095c72e5
Ronaldine <121630753+RonaldineP@users.noreply.github.com>
2023-05-12 23:29:29 +0000

    data modeling

--- src/models.py
+++ src/models.py
@@ -7,6 +7,40 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
+
+   
+
+class Character(Base):
+    __tablename__='character'
+    id = Column(Integer,primary_key=True)
+    name = Column(String(250),nullable=False)
+    age = Column(Integer,nullable=False) 
+    height = Column(String(250),nullable=False)
+    eye_color = Column(String(250), nullable=False)
+    
+
+class Planets(Base):
+    __tablename__='planets'
+    id = Column(Integer,primary_key=True)
+    name =  Column(String(250),nullable=False)
+  
+
+class User(Base):
+    __tablename__= 'user'
+    id = Column(Integer,primary_key=True)
+    name= Column(String(250),nullable=False)
+    email= Column(String(250), nullable=False)
+    phone= Column(String(250),nullable=False)
+    password = Column(String(250),nullable=False)
+    character_id = Column(Integer, ForeignKey('character.id'))
+    planets_id = Column(Integer, ForeignKey('planets.id')) 
+
+class Favorites(Base):
+    __tablename__='favorites'
+    id = Column(Integer,primary_key=True)
+    favorites_id = Column(Integer,ForeignKey('user.id'))
+ 
+    
 class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
